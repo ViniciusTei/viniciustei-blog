ba9dc1fc1c6b4f39c6163b4318d9495639ae4a29
Vinicius Teixeira <viniciustprates@gmail.com>
2025-04-24 08:27:34 -0300

    refactor: moved the html code to the parser

--- markdown/parser.go
+++ markdown/parser.go
@@ -1,5 +1,7 @@
 package markdown
 
+import "fmt"
+
 type NodeType string
 
 const (
@@ -75,6 +77,44 @@ func (p *Parser) Parse() []*Node {
 		p.advance()
 	}
 	return nodes
+}
+
+func (p *Parser) ToHTML() string {
+	nodes := p.Parse()
+	var html string
+	for _, node := range nodes {
+		switch node.Type {
+		case NodeHeader:
+			html += fmt.Sprintf("<h1>%s</h1>\n", node.Content)
+		case NodeHeader2:
+			html += fmt.Sprintf("<h2>%s</h2>\n", node.Content)
+		case NodeHeader3:
+			html += fmt.Sprintf("<h3>%s</h3>\n", node.Content)
+		case NodeHeader4:
+			html += fmt.Sprintf("<h4>%s</h4>\n", node.Content)
+		case NodeHeader5:
+			html += fmt.Sprintf("<h5>%s</h5>\n", node.Content)
+		case NodeHeader6:
+			html += fmt.Sprintf("<h6>%s</h6>\n", node.Content)
+		case NodeParagraph:
+			html += fmt.Sprintf("<p>%s</p>\n", node.Content)
+		case NodeList:
+			/* TODO:
+			 * The list is redenring wrong, it is wrapping every li inside its
+			 * own ul. Inteads, every li should be inside the same ul.
+			 */
+			html += "<ul>\n"
+			for _, child := range node.Children {
+				html += fmt.Sprintf("<li>%s</li>\n", child.Content)
+			}
+			html += "</ul>\n"
+		default:
+			html += fmt.Sprintf("<p>%s</p>\n", node.Content)
+		}
+
+	}
+
+	return html
 }
 
 func (p *Parser) peek() Token {
