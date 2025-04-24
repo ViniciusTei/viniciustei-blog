878426c565722fa56610a76f60459ee9de60271a
Vinicius Teixeira <viniciustprates@gmail.com>
2025-04-24 18:21:22 -0300

    test: parse unordered and ordered lists

--- tests/parse_markdow_to_html_test.go
+++ tests/parse_markdow_to_html_test.go
@@ -10,10 +10,39 @@ func TestParseMarkdownToHTML(t *testing.T) {
 	markdownStr := `# Header
 
   This is a paragraph.
+  - List item 1
+  - List item 2
+
+  * List item 1
+  * List item 2
+
+  + List item 1
+  + List item 2
+
+  1. List item 1
+  2. List item 2
+  3. List item 1
 `
 
 	expected := `<h1>Header</h1>
 <p>This is a paragraph.</p>
+<ul>
+<li>List item 1</li>
+<li>List item 2</li>
+</ul>
+<ul>
+<li>List item 1</li>
+<li>List item 2</li>
+</ul>
+<ul>
+<li>List item 1</li>
+<li>List item 2</li>
+</ul>
+<ol>
+<li>List item 1</li>
+<li>List item 2</li>
+<li>List item 1</li>
+</ol>
 `
 
 	html := markdown.MarkdownToHTML(markdownStr)
