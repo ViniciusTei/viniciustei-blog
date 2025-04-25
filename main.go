8787ef012b1b1f62ad9ad3b7813b98a78b84a734
Vinicius Teixeira <viniciustprates@gmail.com>
2025-04-25 08:00:20 -0300

    feat: add logger to every request to the server

--- main.go
+++ main.go
@@ -3,11 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/ViniciusTei/viniciustei-blog/handlers"
 	"github.com/ViniciusTei/viniciustei-blog/repositories"
 	"github.com/ViniciusTei/viniciustei-blog/usecases"
 )
+
+type Logger struct {
+	handler http.Handler
+}
+
+func (l *Logger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	l.handler.ServeHTTP(w, r)
+	currentTime, err := time.Parse(time.RFC822, time.Now().Format(time.RFC822))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("[%s]: %s %s %v\n", currentTime, r.Method, r.URL.Path, time.Since(start))
+}
+
+func NewLogger(handler http.Handler) *Logger {
+	return &Logger{handler: handler}
+}
 
 func main() {
 	articleRepo := &repositories.ArticleRepositoryImpl{}
@@ -23,6 +43,9 @@ func main() {
 	mux.HandleFunc("/article/{slug}", handler.HandleArticles)
 	mux.HandleFunc("/about", handlers.HandleAbout)
 
+	//middlewares
+	loggerMux := NewLogger(mux)
+
 	fmt.Println("Starting server on :8080")
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":8080", loggerMux)
 }
